cd60e6630dd99c51f3c20b59c191e105277703eb
Coffie <urley.coffie@kpn.com>
2018-11-26 13:34:19 +0100

    add lines

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld
 {
   public static void main(String[] args)
   {
-    System.out.println("hello world aliens");
+    System.out.println("hello world aliens in this world");
   }
 }
